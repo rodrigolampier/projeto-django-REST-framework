c0367a1b87beef6ed5911a43c37f9b31e3553c78
rodrigolampier <rodrigolampier@outlook.com>
2021-01-13 01:11:47 -0300

    Ajusta projeto para deploy Heroku.

--- pontos_turisticos/settings.py
+++ pontos_turisticos/settings.py
@@ -133,3 +133,5 @@ MEDIA_ROOT = 'imagens'
 MEDIA_URL = '/media/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
