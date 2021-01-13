35b06f75c0374dbfc4a50eac03505c48487c4feb
rodrigolampier <rodrigolampier@outlook.com>
2021-01-13 00:56:17 -0300

    Ajusta projeto para deploy Heroku.

--- pontos_turisticos/settings.py
+++ pontos_turisticos/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 from decouple import config
 from dj_database_url import parse as dburl
@@ -21,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = 'sp4t04or%-5x7grp=kn3tg9eou_tkjcb-v&)^pcl15c=hqhizd'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = False
 
 ALLOWED_HOSTS = ['lampier-teste-api-rest.herokuapp.com', 'localhost']
 
